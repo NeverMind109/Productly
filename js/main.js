30d5c2a8b758ce20d4b9518db1787441303457de
NeverMind109 <vladgladkih123@gmail.com>
2021-12-12 21:50:09 +0300

    Created: Added Adaptive Response

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
 $(function(){
+    // slick slider
     $('.slider').slick({
         dots: true,
         arrows: false,
         slidesToShow: 3,
         slidesToScroll: 3,
-    })
+        responsive: [
+            {
+              breakpoint: 1175,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              },
+        ]
+    });
+
+    // burger menu
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active')
+    }); 
+
+    // Плавный переход по ссылкам
+    $("a.tools__link").on("click", function(e){
+        e.preventDefault();
+        var anchor = $(this).attr('href');
+        $('html, body').stop().animate({
+            scrollTop: $(anchor).offset().top - 60
+        }, 1000);
+    });
 });
